97a04ab8c917af86804e471990f8c99a25299f8c
JiajunFanJJ <55294567+JiajunFanJJ@users.noreply.github.com>
2020-02-25 21:47:23 -0800

    Update Web.js

--- Web.js
+++ Web.js
@@ -19,21 +19,22 @@ var pages = {
     rightPage: "life"
   },
   ontheship: {
-    text: "Welcome aboard the Hoo-Hoo ship! We have a bad news, Hoo-Hoo ship is having a maintenance. We are so sorry about that.",
+    text:
+      "Welcome aboard the Hoo-Hoo ship! We have a bad news, Hoo-Hoo ship is having a maintenance. We are so sorry about that.",
     leftLabel: "Go to Train",
     rightLabel: "Quit",
     leftPage: "onthetrain",
     rightPage: "death"
   },
   death: {
-    text:"You failed on this trip.",
-    Label: "Game Over",
-    page: "start"
+    text: "You failed on this trip.",
+    EndLabel: "Game Over",
+    EndPage: "start"
   },
   life: {
-    text:"Wait for your next trip.",
-    Label: "Game Over",
-    page: "start"
+    text: "Wait for your next trip.",
+    EndLabel: "Game Over",
+    EndPage: "start"
   }
 };
 
@@ -55,13 +56,41 @@ class App extends Component {
   render() {
     var pageData = pages[this.state.page];
 
-    return (
-      <div className="App">
-        <img src={pageData.image} alt="page-specific i.amge,"/>
-        <p>{pageData.text}</p>
+    var button1;
+    var button2;
+    var button3;
+
+    if (pageData.leftPage) {
+      button1 = (
         <button onClick={() => this.goToPage(pageData.leftPage)}>
           <p>{pageData.leftLabel}</p>
         </button>
+      );
+    }
+
+    if (pageData.leftPage) {
+      button2 = (
+        <button onClick={() => this.goToPage(pageData.rightPage)}>
+          <p>{pageData.rightLabel}</p>
+        </button>
+      );
+    }
+
+    if (pageData.EndPage) {
+      button3 = (
+        <button onClick={() => this.goToPage(pageData.EndPage)}>
+          <p>{pageData.EndLabel}</p>
+        </button>
+      );
+    }
+
+    return (
+      <div className="App">
+        <img src={pageData.image} alt="page-specific i.amge," />
+        <p>{pageData.text}</p>
+        {button1}
+        {button2}
+        {button3}
       </div>
     );
   }
