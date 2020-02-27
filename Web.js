b372f6466bf3c882d6bc3565d7503eb1bb12378c
JiajunFanJJ <55294567+JiajunFanJJ@users.noreply.github.com>
2020-02-26 19:22:13 -0800

    Update Web.js

--- Web.js
+++ Web.js
@@ -5,6 +5,7 @@ import "./styles.css";
 var pages = {
   start: {
     text: "Welcome, traveler! How would you like to get to your destination?",
+    img: "https://bsmedia.business-standard.com/_media/bs/img/article/2015-08/31/full/1441037121-004.jpg",
     leftLabel: "Train",
     rightLabel: "Ship",
     midLabel: "You suddenly feel so tired and give up.",
@@ -15,6 +16,7 @@ var pages = {
   onthetrain: {
     text:
       "Welcome aboard the Choo-Choo train! Please make your way to your seat. What's the number?",
+    img: "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F9%2F8%2F5%2F5%2F23965589-4-eng-GB%2FCropped-1576270179TX5344P121319%E3%81%97%E3%81%8B%E3%81%91P%EF%BC%89JR%E6%9D%B1%E6%B5%B7%E3%81%AE%E3%81%9E%E3%81%BF%E2%91%A120191213144914196_Data.jpg?source=nar-cms",
     leftLabel: "8B",
     rightLabel: "14E",
     leftPage: "Girl",
@@ -23,6 +25,7 @@ var pages = {
   ontheship: {
     text:
       "Welcome aboard the Hoo-Hoo ship! We have a bad news, Hoo-Hoo ship is having a maintenance. We are so sorry about that.",
+    img: "https://lh3.googleusercontent.com/proxy/thmT4cn9BLGKOh9aKfhlIBPBIt0joTv8oImxZ8-yfvkYQVQYNedxqfn8Ak7_BD3DlJEkdDay9-3PmN7nwrf-MZnjENDvNxnFqV1yp_Nz",
     leftLabel: "Go to Train",
     rightLabel: "Quit",
     leftPage: "onthetrain",
@@ -101,7 +104,11 @@ class App extends Component {
     var button2;
     var button3;
     var button4;
+    var image;
 
+    if (pageData.img) {
+      image = <img src={pageData.img} alt="Haha" width="300" height="250" />;
+    }
     if (pageData.leftPage) {
       button1 = (
         <button onClick={() => this.goToPage(pageData.leftPage)}>
@@ -136,7 +143,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <img src="https://lh3.googleusercontent.com/proxy/thmT4cn9BLGKOh9aKfhlIBPBIt0joTv8oImxZ8-yfvkYQVQYNedxqfn8Ak7_BD3DlJEkdDay9-3PmN7nwrf-MZnjENDvNxnFqV1yp_Nz" alt="Haha" width="300" height="250"/>
+        {image}
         <p>{pageData.text}</p>
         {button1}
         {button2}
